b79c13c31190e6ca5c52600ed93dd9be7564db4d
kid4211 <kid4211@gmail.com>
2021-02-10 17:49:58 +0900

    210210 17:50 Upload Form 소스 업로드

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,30 @@
 import React, { Suspense } from 'react';
-import { Route, Switch } from "react-router-dom";
-import Auth from "../hoc/auth";
+import { Route, Switch } from 'react-router-dom';
+import Auth from '../hoc/auth';
 // pages for this product
-import LandingPage from "./views/LandingPage/LandingPage.js";
-import LoginPage from "./views/LoginPage/LoginPage.js";
-import RegisterPage from "./views/RegisterPage/RegisterPage.js";
-import NavBar from "./views/NavBar/NavBar";
-import Footer from "./views/Footer/Footer"
-
-//null   Anyone Can go inside
-//true   only logged in user can go inside
-//false  logged in user can't go inside
+import LandingPage from './views/LandingPage/LandingPage.js';
+import LoginPage from './views/LoginPage/LoginPage.js';
+import RegisterPage from './views/RegisterPage/RegisterPage.js';
+import NavBar from './views/NavBar/NavBar';
+import Footer from './views/Footer/Footer';
+import VideoUploadPage from './views/ViedoUploadPage/VideoUploadPage';
 
 function App() {
-  return (
-    <Suspense fallback={(<div>Loading...</div>)}>
-      <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
-        <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null)} />
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
-        </Switch>
-      </div>
-      <Footer />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <NavBar />
+            <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+                <Switch>
+                    {/* null : 아무나 접근가능, false : 비로그인만 접근 가능, true : 로그인을 해야 접근 가능 */}
+                    <Route exact path="/" component={Auth(LandingPage, null)} />
+                    <Route exact path="/login" component={Auth(LoginPage, false)} />
+                    <Route exact path="/register" component={Auth(RegisterPage, false)} />
+                    <Route exact path="/video/upload" component={Auth(VideoUploadPage, true)} />
+                </Switch>
+            </div>
+            <Footer />
+        </Suspense>
+    );
 }
 
 export default App;
